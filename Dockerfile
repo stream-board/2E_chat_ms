e97fd0ae9a48b20b83a5bd40009247759e0ea107
Laura Cárdenas <laura.cardenas@axiacore.com>
2018-03-08 16:26:18 -0500

    :sparkles: notify when a user join to a room

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ ENV PYTHONUNBUFFERED 1
 ENV REDIS_HOST "redis"
 RUN mkdir /code
 WORKDIR /code
-ADD . /code/
+COPY . /code/
 RUN pip install -r requirements.txt
 RUN python3.6 manage.py migrate
--- pj_chat_ms/consumers.py
+++ pj_chat_ms/consumers.py
@@ -5,6 +5,7 @@ from asgiref.sync import async_to_sync
 from .models import ChatMessage
 
 GROUP_ROOM_CHAT = 'chat-{id}'
+JOIN_MESSAGE = '{user} se ha unido a la sala'
 
 class ChatConsumer(JsonWebsocketConsumer):
     def connect(self):
@@ -17,32 +18,44 @@ class ChatConsumer(JsonWebsocketConsumer):
             )
         async_to_sync(self.accept())
 
-
     def receive_json(self, event):
+        category = event.get('category')
         room_id = event.get('room_id')
         sender = event.get('sender')
-        message = event.get('message')
+        message = event.get('message', '')
 
-        async_to_sync(
-            ChatMessage.objects.create(
-                chat_room_id=room_id,
-                sender=sender,
-                message=message,
+        if category == 'NEW-MESSAGE':
+            async_to_sync(
+                ChatMessage.objects.create(
+                    chat_room_id=room_id,
+                    sender=sender,
+                    message=message,
+                )
             )
-        )
 
-        async_to_sync(
-            self.channel_layer.group_send)(
-                GROUP_ROOM_CHAT.format(
-                    id=event['room_id']
-                ),
-                {
-                    'type': 'chat.message',
-                    'message': message,
-                    'room_id': room_id,
-                    'sender': sender,
-                },
-            )
+            async_to_sync(
+                self.channel_layer.group_send)(
+                    GROUP_ROOM_CHAT.format(
+                        id=event['room_id']
+                    ),
+                    {
+                        'type': 'chat.message',
+                        'message': message,
+                        'room_id': room_id,
+                        'sender': sender,
+                    },
+                )
+        elif category == 'JOIN-ROOM':
+            async_to_sync(
+                self.channel_layer.group_send)(
+                    GROUP_ROOM_CHAT.format(
+                        id=event['room_id']
+                    ),
+                    {
+                        'type': 'chat.join',
+                        'sender': sender,
+                    },
+                )
 
     def chat_message(self, event):
         room_id = event.get('room_id')
@@ -50,11 +63,20 @@ class ChatConsumer(JsonWebsocketConsumer):
         message = event.get('message')
 
         self.send_json({
+            'category': 'NEW-MESSAGE',
             'room_id': room_id,
             'sender': sender,
             'message': message,
         })
 
+    def chat_join(self, event):
+        sender = event.get('sender')
+        self.send_json({
+            'category': 'JOIN-ROOM',
+            'message': JOIN_MESSAGE.format(
+                user=sender,
+            ),
+        })
 
     def disconnect(self, code):
         async_to_sync(
