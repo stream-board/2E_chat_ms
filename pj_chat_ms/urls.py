69f9f0aa0946498bbf32f7e4bb6cdc0b23c449b7
Laura Cárdenas <laura.cardenas@axiacore.com>
2018-03-19 14:33:13 -0500

    :sparkles::whale: change containers name and delete chatRoom

--- pj_chat_ms/urls.py
+++ pj_chat_ms/urls.py
@@ -5,13 +5,11 @@ from django.urls import path
 from .views import index
 from .views import ChatRoomView
 from .views import ChatRoomCreateView
-from .views import ChatRoomDeleteView
 
 urlpatterns = [
     path('', index, name='index'),
     path('chat-room/<int:room_id>/', ChatRoomView.as_view(), name='chat'),
     path('chat-room/', ChatRoomCreateView.as_view(), name='create_chat_room'),
-    path('chat-room/<int:room_id>/', ChatRoomDeleteView.as_view(), name='delete_chat_room'),
     path('admin/', admin.site.urls),
 
 ]
--- pj_chat_ms/views.py
+++ pj_chat_ms/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import CreateView
-from django.views.generic import TemplateView
+import json
+
 from django.views.generic import View
 
 from django.http import JsonResponse
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404
 
 from .models import ChatRoom
 from .models import ChatMessage
+
 
 #test
 def index(request):
@@ -22,36 +23,37 @@ class ChatRoomView(View):
         chat_room = get_object_or_404(ChatRoom, id=kwargs['room_id'])
         chat_messages = ChatMessage.objects.filter(
             chat_room=chat_room,
-        );
+        )
 
         return JsonResponse([{
-                'id': chat_message.id,
-                'sender': chat_message.sender,
-                'message': chat_message.message
-            } for chat_message in chat_messages],
+            'id': chat_message.id,
+            'sender': chat_message.sender,
+            'message': chat_message.message
+        } for chat_message in chat_messages],
             safe=False
         )
 
-
-class ChatRoomCreateView(View):
-    def post(self, request, *args, **kwargs):
-        print(request.POST)
-        chat_room_id = request.POST.get('id', 0)
-
-        if chat_room_id:
-            chat_room = ChatRoom(id=chat_room_id)
-            chat_room.save()
-
-        return JsonResponse({
-            'id': chat_room_id
-        })
-
-
-class ChatRoomDeleteView(View):
     def delete(self, request, *args, **kwargs):
-        chat_room = get_object_or_404(ChatRoom, chat_room_id=kwargs['room_id'])
+        chat_room = get_object_or_404(ChatRoom, id=kwargs['room_id'])
         chat_room.delete()
 
         return JsonResponse({
             'id': kwargs['room_id']
+        })
+
+
+class ChatRoomCreateView(View):
+    def post(self, request, *args, **kwargs):
+        chat_room_obj = json.loads(request.body)
+        chat_room = None
+
+        if (
+            chat_room_obj['id'] and
+            not ChatRoom.objects.filter(id=chat_room_obj['id']).exists()
+        ):
+            chat_room = ChatRoom(id=chat_room_obj['id'])
+            chat_room.save()
+
+        return JsonResponse({
+            'id': chat_room.id if chat_room else 'Room exits'
         })
