af040c9d3a1f14760fef037e6e3bd15dce68934d
Laura Cárdenas <lacardenasv@unal.edu.co>
2018-05-01 19:02:21 -0500

    middleware removed

--- pj_chat_ms/settings.py
+++ pj_chat_ms/settings.py
@@ -38,18 +38,13 @@ INSTALLED_APPS =  PROJECT_APPS + [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'pj_chat_ms.urls'
